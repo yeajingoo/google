cabec04a8a00918a9c94d87cc9109dea28ff1740
Goo yejin <k7d799@naver.com>
2019-05-05 08:19:37 +0000

    goo

---
+++ google/new/.~c9_invoke_ks6QgM.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class student(models.Model):
+    name = models.CharField(max_length=200)
+    age = models.IntegerFiel
+    age = models.IntegerField()
+    description = models.TextField()
+    
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+      return self.title
+    
